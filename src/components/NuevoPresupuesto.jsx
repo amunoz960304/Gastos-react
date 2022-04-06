a4360b4953177cbc5dc7335e3515584ea03d19d6
amunoz9603 <amunoz@3dadv.mx>
2022-04-06 18:48:14 -0500

    first commit

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import Mensaje from './Mensaje'
+
+const NuevoPresupuesto = ({ 
+    presupuesto, 
+    setPresupuesto,
+    setIsValidPresupuesto 
+}) => {
+
+    const [error, setError] = useState('')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+
+        if(!(presupuesto) || (presupuesto) < 0){
+            setError('No es un presupuesto valido.')
+            return
+        }
+
+        setError('')
+        setIsValidPresupuesto(true)
+    }
+    
+    return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
+            <div className='campo'>
+                <label htmlFor="">Definir Presupuesto</label>
+                <input 
+                    type="number" 
+                    className='nuevo-presupuesto'
+                    placeholder='Añade nuevo presupuesto'
+                    value={presupuesto}
+                    onChange={ e => setPresupuesto(Number(e.target.value))}
+                />
+            </div>
+            <input 
+                type="submit" 
+                value='Añadir'
+            />
+
+        { error && <Mensaje tipo={error}>{error}</Mensaje>}
+
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
